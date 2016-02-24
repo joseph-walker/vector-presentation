b8c47dc9606a85a79336582b62ab60ba4544b9c2
Joseph Walker <joseph@gravity4.com>
2016-02-24 16:01:28 -0600

    Added example output to index.php

--- index.php
+++ index.php
@@ -18,11 +18,14 @@ $data = [
 
 // HTML Table
 print_r($builder->build($data)); echo PHP_EOL;
+// <table><thead><tr><th class="pull-left">Field A</th><th>Field B</th></tr></thead><tbody><tr><td class="pull-left">Hello</td><td>World</td></tr><tr><td class="pull-left">Foo</td><td>Bar</td></tr></tbody></table>
 
 // CSV Table
 $builder->setTablePresenter(new Presenter\CsvPresenter());
 print_r($builder->build($data)); echo PHP_EOL;
+// Field A,Field B\nHello,World\nFoo,Bar
 
 // JSON Table
 $builder->setTablePresenter(new Presenter\JsonPresenter());
 print_r($builder->build($data)); echo PHP_EOL;
+// [{"Field A":"Hello","Field B":"World"},{"Field A":"Foo","Field B":"Bar"}]
