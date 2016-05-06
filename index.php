6f5fec196d8084595bea318e0287be18d91c67ca
Logan Henson <logan@loganhenson.com>
2016-05-06 12:13:15 -0500

    added flex table presenter

--- index.php
+++ index.php
@@ -5,18 +5,31 @@ use Vector\TableBuilder\Presenter;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$builder = new Builder(new Presenter\TablePresenter());
+$builder = new Builder(new Presenter\FlexTablePresenter());
 
 $builder
-    ->column('Field A', ['class' => 'pull-left'], function($a) { return $a['a']; })
+    ->column('Field A', [], function($a) { return $a['a']; })
     ->column('Field B', [], function($b) { return $b['b']; });
 
 $data = [
     ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
+    ['a' => 'Hello', 'b' => 'World'],
     ['a' => 'Foo', 'b' => 'Bar']
 ];
 
-// HTML Table
+// Flex Table
 print_r($builder->build($data)); echo PHP_EOL;
 // <table><thead><tr><th class="pull-left">Field A</th><th>Field B</th></tr></thead><tbody><tr><td class="pull-left">Hello</td><td>World</td></tr><tr><td class="pull-left">Foo</td><td>Bar</td></tr></tbody></table>
 
---
+++ src/TableBuilder/Presenter/FlexTablePresenter.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Vector\TableBuilder\Presenter;
+
+use Vector\Core\Module;
+use Vector\Control\Functor;
+use Vector\Markup\Html;
+
+class FlexTablePresenter extends Module implements PresenterInterface
+{
+    private $tableProps;
+
+    public function __construct($tableProps = [])
+    {
+        $this->tableProps = array_merge([
+            'class' => 'vector_flex_table',
+            'style' => 'display: flex;flex-direction: column;'
+        ], $tableProps);
+    }
+
+    public function build($columnDefinitions, $data)
+    {
+        $node = Html::using('node');
+
+        $table = $node('div');
+
+        $makeHead = self::using('makeHead');
+        $makeBody = self::using('makeBody');
+
+        return $table($this->tableProps, [
+            $makeHead($columnDefinitions),
+            $makeBody($columnDefinitions, $data)
+        ]);
+    }
+
+    protected static function makeHeadCell($column)
+    {
+        $text = Html::using('text');
+        $node = Html::using('node');
+
+        $th = $node('div');
+
+        return $th($column['props'], [$text($column['name'])]);
+    }
+
+    protected static function makeHead($columns)
+    {
+        $map          = Functor::using('fmap');
+        $node         = Html::using('node');
+        $makeHeadCell = self::using('makeHeadCell');
+
+        $thead = $node('div');
+        $tr    = $node('div');
+
+        return $thead([], [
+            $tr([
+                'class' => 'vector_flex_table_head',
+                'style' => 'display: flex;justify-content: space-around;'
+            ], $map($makeHeadCell, $columns))
+        ]);
+    }
+
+    protected static function makeBodyCell($datum, $column)
+    {
+        $text = Html::using('text');
+        $node = Html::using('node');
+
+        $td = $node('div');
+
+        return $td($column['props'], [$text($column['accessor']($datum))]);
+    }
+
+    protected static function makeBodyRow($columns, $datum)
+    {
+        $map          = Functor::using('fmap');
+        $node         = Html::using('node');
+        $makeBodyCell = self::using('makeBodyCell');
+
+        $tr = $node('div');
+
+        return $tr([
+            'class' => 'vector_flex_table_row',
+            'style' => 'display: flex;justify-content: space-around;'
+        ], $map($makeBodyCell($datum), $columns));
+    }
+
+    protected static function makeBody($columns, $data)
+    {
+        $map         = Functor::using('fmap');
+        $node        = Html::using('node');
+        $makeBodyRow = self::using('makeBodyRow');
+
+        $tbody = $node('div');
+
+        return $tbody([
+            'class' => 'vector_flex_table_body',
+            'style' => 'display: flex;flex-direction: column;'
+        ], $map($makeBodyRow($columns), $data));
+    }
+}
