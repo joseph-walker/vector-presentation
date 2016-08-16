51146ca6fc335935d8cba3cf7b6b703b73d137dd
Logan Henson <logan@loganhenson.com>
2016-08-16 10:15:51 -0500

    removed themes

--- bootstrap.php
+++ bootstrap.php
@@ -1,32 +1,3 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-
-use Illuminate\Container\Container;
-use Illuminate\Database\Capsule\Manager;
-use Illuminate\Database\Eloquent\Model as Eloquent;
-use Illuminate\Events\Dispatcher;
-use Vector\Setup;
-
-call_user_func(function () {
-    $capsule = new Manager();
-    $capsule->addConnection([
-        'driver'    => 'mysql',
-        'host'      => 'localhost',
-        'database'  => 'homestead',
-        'username'  => 'root',
-        'password'  => 'root',
-        'charset'   => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'prefix'    => '',
-    ]);
-    $capsule->setAsGlobal();
-    $capsule->bootEloquent();
-
-    class User extends Eloquent
-    {
-        protected $table = 'users';
-    }
-});
-
-Setup::eloquentPagination();
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Vector\FormBuilder\Builder;
-use Vector\FormBuilder\Presenter\PrimePresenter;
-
-require (__DIR__ . '/vendor/autoload.php');
